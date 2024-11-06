872b29ea8719da6fd75bd914b41299871fc2d384
Nagalakshmi <nagalakshmi.90246@gmail.com>
2024-11-06 17:32:46 +0530

    246

---
+++ B2B_SwarajLease/src/test/java/com/swaraj/testcases/TC2_AddPropertyTest.java
@@ -0,0 +1,40 @@
+package com.swaraj.testcases;
+
+import java.io.IOException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.swaraj.pageobjects.loginPage;
+import com.swaraj.pageobjects.welcomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC2_AddPropertyTest extends BaseClass{
+	
+	@Test
+	public void VerifyLogin() throws IOException 
+	{
+
+		logger.info("***************TestCase Verify Login starts*****************"); 
+
+		welcomePage wpg = new welcomePage(driver);
+
+		wpg.clickOnLogin();
+		logger.info("Clicked on Login link");
+
+		loginPage loginpg = new loginPage(driver);
+
+		loginpg.enterEmailAddress("newuser@gmail.com");
+		logger.info("Entered email address");
+
+		loginpg.enterPassword("newuser@gmail.com@1");
+		logger.info("Entered password");
+
+		loginpg.clickLogIn();
+		logger.info("Clicked on login button..");
+		
+
+
+
+	}
+
+}
