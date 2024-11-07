5f7f0b9b239d538eefc14dfe6f518a3c83387ec2
Nagalakshmi <nagalakshmi.90246@gmail.com>
2024-11-07 18:16:23 +0530

    propertypage

--- B2B_SwarajLease/src/test/java/com/swaraj/pageobjects/propertyPage.java
+++ B2B_SwarajLease/src/test/java/com/swaraj/pageobjects/propertyPage.java
@@ -18,83 +18,108 @@ public class propertyPage {
 	}
 
 	//identify webelements
-	@FindBy(xpath = "(//a[@class='topBarHead'])[1]") 
-	WebElement property_tab;
-	
-	@FindBy(xpath = "//button[normalize-space()='Add Property']//*[name()='svg']") 
-	WebElement addproperty_btn;
-	
-	@FindBy(xpath = "//input[@id='propertyName']") 
-	WebElement propName_txt1;
-	@FindBy(xpath = "//textarea[@id='propertyAddress']") 
-	WebElement propAdds_txt;
-	@FindBy(xpath = "//span[normalize-space()='Select Type']") 
-	WebElement type_DD;
-	@FindBy(xpath = "//div[@class=' css-78rtvi']") 
-	WebElement state_DD;
-	@FindBy(xpath = "//span[normalize-space()='Select Location']") 
-	WebElement location_DD;
-	
-	@FindBy(xpath = "//input[@id='unitName']") 
-	WebElement unitName_txt;
-	@FindBy(xpath = "//textarea[@id='unitDesc']") 
-	WebElement unitDesc_txt;
-	@FindBy(id = "unitRentPerMonth")
-	WebElement unitRentPM_txt;
-	@FindBy(xpath = "//input[@id='unitInc']")
-	WebElement unitIncPrct_txt;
-	
-	@FindBy(xpath = "//body/div[@id='modal-hook']/div[@class='modal-container modal-centered property-modal']/form[@class='modal-scrollable mt-4']/div[1]/div[1]/div[1]/input[1]") 
-	WebElement areaSqft_txt;
-	@FindBy(xpath = "(//input[@class=\"label-content\"])[5]") 
-	WebElement marketRate_txt;
-	
-	
-	//identify action on webelement
-	
-	public void enterPropName(String propName) 
-	{
-		propName_txt1.sendKeys(propName);
-	}
+		@FindBy(xpath = "(//a[@class='topBarHead'])[1]") 
+		WebElement property_tab;
+		
+		@FindBy(xpath = "//button[normalize-space()='Add Property']//*[name()='svg']") 
+		WebElement addproperty_btn;
+		
+		@FindBy(xpath = "//input[@id='propertyName']") 
+		WebElement propName_txt1;
+		@FindBy(xpath = "//textarea[@id='propertyAddress']") 
+		WebElement propAdds_txt;
+		@FindBy(xpath = "//span[normalize-space()='Select Type']") 
+		WebElement type_DD;
+		@FindBy(xpath = "//div[@class=' css-78rtvi']") 
+		WebElement state_DD;
+		@FindBy(xpath = "//span[normalize-space()='Select Location']") 
+		WebElement location_DD;
+		
+		@FindBy(xpath = "//input[@id='unitName']") 
+		WebElement unitName_txt;
+		@FindBy(xpath = "//textarea[@id='unitDesc']") 
+		WebElement unitDesc_txt;
+		@FindBy(id = "unitRentPerMonth")
+		WebElement unitRentPM_txt;
+		@FindBy(xpath = "//input[@id='unitInc']")
+		WebElement unitIncPrct_txt;
+		
+		@FindBy(xpath = "//body/div[@id='modal-hook']/div[@class='modal-container modal-centered property-modal']/form[@class='modal-scrollable mt-4']/div[1]/div[1]/div[1]/input[1]") 
+		WebElement areaSqft_txt;
+		@FindBy(xpath = "(//input[@class=\"label-content\"])[5]") 
+		WebElement marketRate_txt;
+		@FindBy(xpath = "//button[normalize-space()='Add Another Unit']") 
+		WebElement AddAnotherUnit_btn;
+		
+		
+		//identify action on webelement
+		public void clickPropTab() 
+		{
+			property_tab.click();
+		}
+		
+		public void clickAddproperty_btn() 
+		{
+			addproperty_btn.click();
+		}
+		
+		public void enterPropName(String propName) 
+		{
+			propName_txt1.sendKeys(propName);
+		}
 
 
-	public void enterPropAdds(String propAdds) 
-	{
-		propAdds_txt.sendKeys(propAdds);
-	}
-	
-	public void selectType(String text) 
-	{
-		Select obj1=new Select(type_DD);
-		obj1.selectByVisibleText(text);
-	}
-	
-	public void selectState(String text) 
-	{
-		Select obj2=new Select(state_DD);
-		obj2.selectByVisibleText(text);
-	}
-	
-	public void selectLocation(String text) 
-	{
-		Select obj3=new Select(location_DD);
-		obj3.selectByVisibleText(text);
-	}
-	
-	public void enterunitName_txt(String unitName) 
-	{
-		unitName_txt.sendKeys(unitName);
-	}
-	
-	public void enterunitDesc_txt(String unitDes) 
-	{
-		unitDesc_txt.sendKeys(unitDes);
-	}
-	
-	public void enterunitIncPrct_txt(String unitInt) 
-	{
-		unitIncPrct_txt.sendKeys(unitInt);
-	}
+		public void enterPropAdds(String propAdds) 
+		{
+			propAdds_txt.sendKeys(propAdds);
+		}
+		
+		public void selectType(String text) 
+		{
+			Select obj1=new Select(type_DD);
+			obj1.selectByVisibleText(text);
+		}
+		
+		public void selectState(String text) 
+		{
+			Select obj2=new Select(state_DD);
+			obj2.selectByVisibleText(text);
+		}
+		
+		public void selectLocation(String text) 
+		{
+			Select obj3=new Select(location_DD);
+			obj3.selectByVisibleText(text);
+		}
+		
+		public void enterunitName_txt(String unitName) 
+		{
+			unitName_txt.sendKeys(unitName);
+		}
+		
+		public void enterunitDesc_txt(String unitDes) 
+		{
+			unitDesc_txt.sendKeys(unitDes);
+		}
+		
+		public void enterunitIncPrct_txt(String unitInt) 
+		{
+			unitIncPrct_txt.sendKeys(unitInt);
+		}
+		
+		public void enterareaSqft_txt(String areaSqft) 
+		{
+			areaSqft_txt.sendKeys(areaSqft);
+		}
+		public void entermarketRate_txt(String marketRate) 
+		{
+			unitIncPrct_txt.sendKeys(marketRate);
+		}
+		
+		public void clickAddAnotherUnit_btn() 
+		{
+			AddAnotherUnit_btn.click();
+		}
 
 	
 
--- B2B_SwarajLease/src/test/java/com/swaraj/testcases/TC2_AddPropertyTest.java
+++ B2B_SwarajLease/src/test/java/com/swaraj/testcases/TC2_AddPropertyTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.swaraj.pageobjects.loginPage;
+import com.swaraj.pageobjects.propertyPage;
 import com.swaraj.pageobjects.welcomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,7 +12,7 @@ import org.testng.annotations.Test;
 public class TC2_AddPropertyTest extends BaseClass{
 	
 	@Test
-	public void VerifyLogin() throws IOException 
+	public void VerifyLogin() throws IOException, InterruptedException 
 	{
 
 		logger.info("***************TestCase Verify Login starts*****************"); 
@@ -31,6 +32,29 @@ public class TC2_AddPropertyTest extends BaseClass{
 
 		loginpg.clickLogIn();
 		logger.info("Clicked on login button..");
+		Thread.sleep(5000);
+		
+		propertyPage proppg = new propertyPage(driver);
+		
+		proppg.clickPropTab();
+		logger.info("Clicked on Property tab");
+		
+		proppg.clickAddproperty_btn();
+		logger.info("Clicked on AddProperty button, Add property pop-up displayed");
+		
+		proppg.enterPropName("autoProperty");
+		proppg.enterPropAdds("Udupi");
+		proppg.selectType("Commercial");
+		proppg.selectState("Karnataka");
+		proppg.selectLocation("Loc1");
+		proppg.enterunitName_txt("unit1");
+		proppg.enterunitDesc_txt("shopping complex");
+		proppg.enterunitIncPrct_txt("5");
+		proppg.enterareaSqft_txt("1000");
+		Thread.sleep(5000);
+		proppg.entermarketRate_txt("10");
+		proppg.clickAddAnotherUnit_btn();
+		
 		
 
 
